c8aa7206fb49f6f9c27c033c172a68b74b16644c
Andrew Throener <athroener@mystartupsolutions.com>
2017-05-24 12:44:26 -0500

    Update

--- docs/js/convert.js
+++ docs/js/convert.js
@@ -1,7 +1,6 @@
 // Download, install, and enable this chrome extension:  https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&uact=8&ved=0ahUKEwinv9jPgYnUAhVY2GMKHQh4AOgQFggoMAA&url=https%3A%2F%2Fchrome.google.com%2Fwebstore%2Fdetail%2Fdisable-content-security%2Fieelmcmcagommplceebfedjlakkhpden%3Fhl%3Den&usg=AFQjCNEiw78bxyps59VEQRH2uaEdil7jRQ&sig2=HqJMkvj8VoyVEpDzriKtyg
 // Open: https://developer.paypal.com/docs/integration/direct/express-checkout/integration-jsv4/add-paypal-button/
-// Select All: Ctrl+A
-// Copy All: Ctrl+C
+// Copy Everything in this file: Ctrl+A Ctrl+C
 // Open Chrome Console:  Ctrl+Shift+J
 // Paste: Ctrl+V
 // Enter
