945590ec7c3a219185a14db35ebcf3285925a76f
Andrew Throener <athroener@mystartupsolutions.com>
2017-05-24 12:30:17 -0500

    Update

--- docs/developer.md
+++ docs/developer.md
@@ -2,9 +2,7 @@
 layout: developer
 ---
 <div id="main"></div>
-<script src="/paypal-checkout/js/xcomponent.min.js" type="text/javascript" charset="utf-8"></script> <!-- Remove if we expose xcomponent.create in the checkout.js lib -->
-<script type="text/javascript" src="/paypal-checkout/js/main-component.js"></script>
-<script type="text/javascript">
-    MainComponent.render({}, '#main');
-</script>
+
+<script type="text/javascript" src="/paypal-checkout/js/main-component.js" data-render="true" data-render-id="#main"></script>
+
 
--- docs/js/main-component.js
+++ docs/js/main-component.js
@@ -1,13 +1,42 @@
-var MainComponent = xcomponent.create({
-    tag: 'main-component',
-    url: '//trainerbill.github.io/paypal-checkout/docs/',
-    autoResize: {
-        width: false,
-        height: true,
-        element: 'body'
-    },
-    // domain: 'https://trainerbill.github.io',
-    props: {
-         
+var DocsComponent;
+var DocsComponentScript = document.currentScript;
+
+function initXcomponent() {
+
+    DocsComponent = xcomponent.create({
+        tag: 'docs-component',
+        url: 'https://trainerbill.github.io/paypal-checkout/docs/',
+        dimensions: {
+            width: '800px',
+            height: '800px'
+        },
+        domain: 'https://trainerbill.github.io',
+        // scrolling: false,
+        props: {
+            
+        }
+    });
+
+    if (DocsComponentScript.getAttribute('data-render')) {
+        DocsComponent.render({}, DocsComponentScript.getAttribute('data-render-id'));
     }
-});
+
+}
+
+if (!window.xcomponent) {
+    var xComponent = document.createElement("script");
+    xComponent.src = "https://www.paypalobjects.com/api/xcomponent.min.js";
+    xComponent.onload = function () {
+        if (window.require) {
+            require(['xcomponent'], function (lib) {
+                window.xcomponent = lib;
+                initXcomponent();
+            });
+        } else {
+            initXcomponent();
+        }
+    };
+    document.head.appendChild(xComponent);
+} else {
+    initXcomponent();
+}
